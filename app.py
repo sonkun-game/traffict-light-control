e4ae1cdc29b9ed937ac4a8e881b45817323cdb26
sonht <mikechael74@gmail.com>
2025-05-12 00:11:10 +0700

    finish data

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import os
 import shutil
+import math
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
-from lab import collect_data_from_train_model, collect_data_by_train_model
+from lab import collect_data_by_train_model, readJsonFile
 
 app = Flask(__name__)
 UPLOAD_FOLDER = 'img'
@@ -39,7 +40,61 @@ def get_file(filename):
 def train_data(filename):
     delete_all_in_detect()
     str = collect_data_by_train_model(filename)
-    return str
+    data = str.split(",")
+    result = ""
+    for item in data:
+        item = item.strip()
+        analysis_data = item.split(" ")
+        num = analysis_data[0]
+        text = readJsonFile(analysis_data[1])
+        result += num + " " + text + " <br>"
+    return result
+
+@app.route('/analysis/<filename>',  methods=["POST"])
+def analy_data(filename):
+    saturation_flow = 1.584
+    str = collect_data_by_train_model(filename)
+    data = str.split(",")
+    east = 0
+    north = 0
+    west = 0
+    south = 0
+    intersection = 0
+    for item in data:
+        item = item.strip()
+        analysis_data = item.split(" ")
+        num = int(analysis_data[0])
+        if "intersection" in item : 
+            intersection += num
+        if "east" in item :
+            east += num
+        if "north" in item: 
+            north += num
+        if "west" in item:
+            west += num
+        if "south" in item:
+            south += num
+    total_east_west = east + west
+    total_south_north = south + north
+
+    # Calculate east west green time
+    east_west_green =  math.ceil(total_east_west / saturation_flow) + (intersection * 2) # add 2 more seconds for intersection
+    if east_west_green < 10:
+        east_west_green = 10
+    if east_west_green > 30:
+        east_west_green = 30
+
+    # Calculate north south green time
+    norht_south_green = math.ceil(total_south_north / saturation_flow) + (intersection * 2) # add 2 more seconds for intersection
+    if norht_south_green < 10:
+        norht_south_green = 10
+    if norht_south_green > 30:
+        norht_south_green = 30
+
+    print('------------------')
+    print(east_west_green)
+    print(norht_south_green)
+    return [east_west_green, norht_south_green]
 
 # Get runs image
 @app.route('/runs/<filename>')
@@ -47,7 +102,6 @@ def get_result_image(filename):
     print('-- get_result_image --')
     if "png" in filename:
         filename = filename.replace("png", "jpg")
-        print(filename)
     return send_from_directory(app.config["RUNS_FOLDER"], filename)
 
 @app.route('/')
--- lab.py
+++ lab.py
@@ -1,8 +1,10 @@
 from ultralytics import YOLO
 from collections import Counter
+import json
 
 def collect_data_by_train_model(image_path):
     model = YOLO('./best.pt')
+    image_path = 'img/' + image_path
     result = model(image_path, save=True)[0]
 
     print()
@@ -31,35 +33,9 @@ def collect_data_by_train_model(image_path):
 
     return summary
 
-
-
-def collect_data_from_train_model():
-    model = YOLO('./best.pt')
-    result = model("./test_2.png", save=True)[0]
-
-    print()
-    print("----START HERE----")
-    # Step 1: Get class indices from result
-    class_ids = result.boxes.cls.int().tolist()
-    print("---CLASS ID---")
-    print(class_ids)
-
-    # Step 2: Map class IDs to class names
-    names = result.names
-    detected_names = [names[i] for i in class_ids]
-    print("---DETECTED NAME---")
-    print(detected_names)
-
-    # Step 3: Count occurrences of each class name
-    counts = Counter(detected_names)
-    print("---COUNT---")
-    print(counts)
-
-    # Step 4: Format like "5 car-intersections, 1 east-car, ..."
-    summary = ", ".join(f"{v} {k}" for k, v in counts.items())
-    print("----GET SUMMARY----")
-    print(summary)
-    print()
-
-    return summary
+def readJsonFile(name):
+    with open('traffic.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+    return data[name]
+        
 
